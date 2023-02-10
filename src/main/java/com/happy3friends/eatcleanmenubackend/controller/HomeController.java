af1459f1b891e91d285022653917170e5c125a31
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2023-02-11 00:11:17 +0700

    [tienhuynh-tn] Add HomeController

---
+++ src/main/java/com/happy3friends/eatcleanmenubackend/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.happy3friends.eatcleanmenubackend.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(value = "Home API", description = "Home API", tags = "Home API")
+public class HomeController {
+
+    @ApiOperation(value = "Home")
+    @GetMapping(value = "/")
+    public String home() {
+        return "Hello Eat Clean Menu - tienhuynh-tn";
+    }
+}
