74ffae498da302528cea004084fc2253b42578fd
Huỳnh Lê Thủy Tiên <tien.huynhlt.tn@gmail.com>
2023-02-19 08:58:24 +0700

    Update HomeController.java

--- src/main/java/com/happy3friends/eatcleanmenubackend/controller/HomeController.java
+++ src/main/java/com/happy3friends/eatcleanmenubackend/controller/HomeController.java
@@ -12,6 +12,6 @@ public class HomeController {
     @ApiOperation(value = "Home")
     @GetMapping(value = "/")
     public String home() {
-        return "Hello Eat Clean Menu - tienhuynh-tn";
+        return "Hello 123 Eat Clean Menu - tienhuynh-tn";
     }
 }
